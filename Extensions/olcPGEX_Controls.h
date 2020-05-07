90ebb87564b8246613d3b03401d2d1e768f84eeb
piotrwyrw <64374780+piotrwyrw@users.noreply.github.com>
2020-05-07 07:42:47 +0200

    Update
    Uptade stuff,
    Remove unnecessary stuff.

--- Extensions/olcPGEX_Controls.h
+++ Extensions/olcPGEX_Controls.h
@@ -8,8 +8,7 @@
 	What is this?
 	~~~~~~~~~~~~~
 	This is an extension to the olcPixelGameEngine, which provides
-	basic controls and event
-	handling.
+	some basic controls and event handlers.
 
 	Example
 	~~~~~~~
@@ -94,9 +93,7 @@ namespace olc
 			pge = pixelGameEngine;
 		}
 
-		class BasicControl;
-
-		struct CBoundingBox
+		struct CBox
 		{
 			int x;
 			int y;
@@ -104,14 +101,30 @@ namespace olc
 			int height;
 		};
 
-		CBoundingBox* CreateBoundingBox(int x, int y, int width, int height)
+		CBox* CreateBox(int x, int y, int width, int height)
 		{
-			return new CBoundingBox{ x, y, width, height };
+			return new CBox{ x, y, width, height };
 		}
 
-		/*
-		Handles the events of BasicControls
-		*/
+		bool IsInBounds(CBox* box, int x, int y)
+		{
+			if (x > box->x && x <= box->x + box->width && y >= box->y && y <= box->y + box->height)
+			{
+				return true;
+			}
+			return false;
+		}
+
+		bool IsInBounds(int boxX, int boxY, int boxWidth, int boxHeight, int pointX, int pointY)
+		{
+			if (pointX > boxX && pointX <= boxX + boxWidth && pointY >= boxY && pointY <= boxY + boxHeight)
+			{
+				return true;
+			}
+			return false;
+		}
+
+		class BasicControl;
 		class CEventHandler
 		{
 		public:
@@ -125,6 +138,32 @@ namespace olc
 		{
 		public:
 			std::vector<CEventHandler*> eventHandlers;
+			bool lockHandlers = false;
+			bool lockUpdates = false;
+			void LockHandlers()
+			{
+				lockHandlers = true;
+			}
+			void FreeHandlers()
+			{
+				lockHandlers = false;
+			}
+			bool HandlersLocked()
+			{
+				return lockHandlers;
+			}
+			void LockUpdates()
+			{
+				lockUpdates = true;
+			}
+			void FreeUpdates()
+			{
+				lockUpdates = false;
+			}
+			bool UpdatesLocked()
+			{
+				return lockUpdates;
+			}
 			virtual float GetX()
 			{
 				return 0.0f;
@@ -144,44 +183,41 @@ namespace olc
 			{
 				return 0.0f;
 			}
-			virtual void UpdateControl() {}
+			virtual void UpdateSelf() {}
+			void UpdateControl()
+			{
+				if (!lockUpdates)
+					UpdateSelf();
+			}
 			void AddEventHandler(CEventHandler* evh)
 			{
-				eventHandlers.push_back(evh);
-			}
-			bool IsInBounds(int x, int y, int w, int h, int pointX, int pointY)
-			{
-				if (pointX > x && pointX <= x + w && pointY >= y && pointY <= y + h) {
-					return true;
-				}
-				return false;
-			}
-			bool IsInBounds(CBoundingBox* boundingBox, int pointX, int pointY)
-			{
-				return IsInBounds(boundingBox->x, boundingBox->y, boundingBox->width, boundingBox->height, pointX, pointY);
+				this->eventHandlers.push_back(evh);
 			}
 			void H_CClicked(BasicControl* ctrl)
 			{
 				for (CEventHandler* handler : eventHandlers)
-					handler->CClicked(pge, ctrl);
+					if (!lockHandlers)
+						handler->CClicked(pge, ctrl);
 			}
 			void H_CReleased(BasicControl* ctrl)
 			{
 				for (CEventHandler* handler : eventHandlers)
-					handler->CReleased(pge, ctrl);
+					if (!lockHandlers)
+						handler->CReleased(pge, ctrl);
 			}
 			void H_CHover(BasicControl* ctrl)
 			{
 				for (CEventHandler* handler : eventHandlers)
-					handler->CHover(pge, ctrl);
+					if (!lockHandlers)
+						handler->CHover(pge, ctrl);
 			}
 			void H_CValueChanged(BasicControl* ctrl, float newValue)
 			{
 				for (CEventHandler* handler : eventHandlers)
-					handler->CValueChanged(pge, ctrl, newValue);
+					if (!lockHandlers)
+						handler->CValueChanged(pge, ctrl, newValue);
 			}
 		};
-
 
 		/*
 		--------------------------------------------------
@@ -203,7 +239,8 @@ namespace olc
 			olc::Pixel foregroundColor;
 			olc::Pixel foregroundColorHover;
 			std::string text;
-			CButton(olc::vf2d position = olc::vf2d(0.0f, 0.0f), olc::vf2d size = olc::vf2d(0.0f, 0.0f), std::string text = "CButton", olc::Pixel backgroundColor = olc::Pixel(44, 62, 80), olc::Pixel backgroundColorHover = olc::Pixel(52, 73, 94), olc::Pixel foregroundColor = olc::Pixel(236, 240, 241), olc::Pixel foregroundColorHover = olc::Pixel(236, 240, 241))
+			int32_t textScale;
+			CButton(olc::vf2d position = olc::vf2d(0.0f, 0.0f), olc::vf2d size = olc::vf2d(0.0f, 0.0f), std::string text = "CButton", int32_t textScale = 2U, olc::Pixel backgroundColor = olc::Pixel(44, 62, 80), olc::Pixel backgroundColorHover = olc::Pixel(52, 73, 94), olc::Pixel foregroundColor = olc::Pixel(236, 240, 241), olc::Pixel foregroundColorHover = olc::Pixel(236, 240, 241))
 			{
 				this->x = position.x;
 				this->y = position.y;
@@ -216,6 +253,7 @@ namespace olc
 				this->text = text;
 				this->textXOffset = 0;
 				this->textYOffset = 0;
+				this->textScale = textScale;
 			}
 			void PlaceText(float tpx, float tpy)
 			{
@@ -224,35 +262,35 @@ namespace olc
 			}
 			float GetX() override
 			{
-				return this->x;
+				return x;
 			}
 			float GetY() override
 			{
-				return this->y;
+				return y;
 			}
 			float GetWidth() override
 			{
-				return this->width;
+				return width;
 			}
 			float GetHeight() override
 			{
-				return this->height;
+				return height;
 			}
-			void UpdateControl() override
+			void UpdateSelf() override
 			{
 				olc::Pixel backgroundColor = this->backgroundColor;
 				olc::Pixel foregroundColor = this->foregroundColor;
 
-				CBoundingBox* boundingBox = CreateBoundingBox(x, y, width, height);
+				CBox* boundingBox = CreateBox(x, y, width, height);
 				if (IsInBounds(boundingBox, pge->GetMouseX(), pge->GetMouseY())) {
-					backgroundColor = this->backgroundColorHover;
-					foregroundColor = this->foregroundColorHover;
+					backgroundColor = backgroundColorHover;
+					foregroundColor = foregroundColorHover;
 					H_CHover(this);
 				}
 				pge->FillRect(x, y, width, height, backgroundColor);
-				pge->DrawString(x + textXOffset, y + textYOffset, this->text, foregroundColor, 2);
+				pge->DrawString(x + textXOffset, y + textYOffset, text, foregroundColor, textScale);
 
-				if (IsInBounds(x, y, width, height, pge->GetMouseX(), pge->GetMouseY()))
+				if (IsInBounds(boundingBox, pge->GetMouseX(), pge->GetMouseY()))
 				{
 					if (pge->GetMouse(0).bPressed || pge->GetMouse(1).bPressed || pge->GetMouse(2).bPressed)
 					{
@@ -275,6 +313,7 @@ namespace olc
 			float width;
 			float height;
 			float value;
+			float targetValue = 0.0f;
 			olc::Pixel backgroundColor;
 			olc::Pixel foregroundColor;
 			Orientation orientation;
@@ -300,37 +339,37 @@ namespace olc
 			}
 			float GetX() override
 			{
-				return this->x;
+				return x;
 			}
 			float GetY() override
 			{
-				return this->y;
+				return y;
 			}
 			float GetWidth() override
 			{
-				return this->width;
+				return width;
 			}
 			float GetHeight() override
 			{
-				return this->height;
+				return height;
 			}
 			/* Gets the progress size of horizontal progressbar */
 			float GetProgressWidth()
 			{
-				return (this->width / 100) * value;
+				return (width / 100) * value;
 			}
 			/* Gets the progress size of vertical progressbar */
 			float GetProgressHeight()
 			{
-				return (this->width / 100) * value;
+				return (width / 100) * value;
 			}
 			Orientation GetOrientation()
 			{
-				return this->orientation;
+				return orientation;
 			}
 			void Increment(float incrementValue)
 			{
-				value += incrementValue;
+				targetValue = value + incrementValue;
 				H_CValueChanged(this, value);
 			}
 			/* Only Increments if value in range */
@@ -340,7 +379,7 @@ namespace olc
 					Increment(incrementValue);
 			}
 			void Decrement(float decrementValue) {
-				this->value -= decrementValue;
+				targetValue = value - decrementValue;
 				H_CValueChanged(this, value);
 			}
 			/* Only Decrements if value in range */
@@ -355,7 +394,7 @@ namespace olc
 				{
 					H_CValueChanged(this, value);
 				}
-				value = newValue;
+				targetValue = newValue;
 			}
 			float GetValue()
 			{
@@ -369,9 +408,9 @@ namespace olc
 			{
 				return value <= 0;
 			}
-			void UpdateControl() override
+			void UpdateSelf() override
 			{
-				CBoundingBox* boundingBox = CreateBoundingBox(x, y, width, height);
+				CBox* boundingBox = CreateBox(x, y, width, height);
 				if (IsInBounds(boundingBox, pge->GetMouseX(), pge->GetMouseY()))
 				{
 					H_CHover(this);
@@ -380,18 +419,33 @@ namespace olc
 					if (pge->GetMouse(0).bReleased || pge->GetMouse(1).bReleased || pge->GetMouse(2).bReleased)
 						H_CReleased(this);
 				}
-				float newWidth = this->width;
-				float newHeight = this->height;
+
+				// Smooth progress bars
+				if ((int)value != (int)targetValue)
+				{
+					if ((int)targetValue < (int)value)
+					{
+						if (!IsEmpty())
+							value -= 0.4f;
+					}
+					else if ((int)targetValue > (int)value)
+					{
+						if (!IsFull())
+							value += 0.4f;
+					}
+				}
+				float newWidth = width;
+				float newHeight = height;
 				switch (orientation)
 				{
 				case HORIZONTAL:
-					newWidth = (this->width / 100) * this->value;
+					newWidth = (width / 100) * value;
 					break;
 				case VERTICAL:
-					newHeight = (this->height / 100) * this->value;
+					newHeight = (height / 100) * value;
 					break;
 				}
-				pge->FillRect(x, y, this->width, this->height, backgroundColor);
+				pge->FillRect(x, y, width, height, backgroundColor);
 				pge->FillRect(x, y, newWidth, newHeight, foregroundColor);
 				delete boundingBox;
 			}
@@ -424,27 +478,27 @@ namespace olc
 			}
 			float GetX() override
 			{
-				return this->x;
+				return x;
 			}
 			float GetY() override
 			{
-				return this->y;
+				return y;
 			}
 			float GetWidth() override
 			{
-				return this->size;
+				return size;
 			}
 			float GetHeight() override
 			{
-				return this->size;
+				return size;
 			}
 			void SetHeadOffset(float hoffset)
 			{
-				this->headOffset = hoffset;
+				headOffset = hoffset;
 			}
 			float GetHeadOffset()
 			{
-				return this->headOffset;
+				return headOffset;
 			}
 			float GetPercent()
 			{
@@ -454,11 +508,11 @@ namespace olc
 			{
 				return (maxv / 100) * GetPercent();
 			}
-			void UpdateControl() override
+			void UpdateSelf() override
 			{
 				if (orientation == HORIZONTAL)
 				{
-					CBoundingBox* boundingBox = CreateBoundingBox(this->x + headOffset, this->y - 30 / 2, 10, 30);
+					CBox* boundingBox = CreateBox(x + headOffset, y - 30 / 2, 10, 30);
 					if (IsInBounds(boundingBox, pge->GetMouseX(), pge->GetMouseY()))
 					{
 						H_CHover(this);
@@ -479,21 +533,21 @@ namespace olc
 					if (IsSelected)
 					{
 						int newOffset = pge->GetMouseX() - x;
-						if (newOffset >= -1 && newOffset <= (int)this->size + 1)
+						if (newOffset >= -1 && newOffset <= (int)size + 1)
 						{
-							this->headOffset = newOffset;
+							headOffset = newOffset;
 							H_CValueChanged(this, headOffset);
 						}
 					}
-					pge->FillRect(this->x, this->y, this->size, 5, this->backgroundColor);
-					pge->FillRect(this->x + this->headOffset, this->y - 30 / 2, 10, 30, this->foregroundColor);
+					pge->FillRect(x, y, size, 5, backgroundColor);
+					pge->FillRect(x + headOffset, y - 30 / 2, 10, 30, foregroundColor);
 					delete boundingBox;
 					return;
 				}
 
 				if (orientation == VERTICAL)
 				{
-					CBoundingBox* boundingBox = CreateBoundingBox(this->x - 30 / 2, this->y + headOffset, 30, 10);
+					CBox* boundingBox = CreateBox(x - 30 / 2, y + headOffset, 30, 10);
 					if (IsInBounds(boundingBox, pge->GetMouseX(), pge->GetMouseY()))
 					{
 						H_CHover(this);
@@ -514,14 +568,14 @@ namespace olc
 					if (IsSelected)
 					{
 						int newOffset = pge->GetMouseY() - y;
-						if (newOffset >= -1 && newOffset <= (int)this->size + 1)
+						if (newOffset >= -1 && newOffset <= (int)size + 1)
 						{
-							this->headOffset = newOffset;
+							headOffset = newOffset;
 							H_CValueChanged(this, headOffset);
 						}
 					}
-					pge->FillRect(this->x, this->y, 5, this->size, this->backgroundColor);
-					pge->FillRect(this->x - 30 / 2, this->y + headOffset, 30, 10, this->foregroundColor);
+					pge->FillRect(x, y, 5, size, backgroundColor);
+					pge->FillRect(x - 30 / 2, y + headOffset, 30, 10, foregroundColor);
 					delete boundingBox;
 					return;
 				}
@@ -553,16 +607,16 @@ namespace olc
 				this->textScale = textScale;
 			}
 			void PlaceText(float textXOffset, float textYOffset) {
-				this->textXOffset = textXOffset;
-				this->textYOffset = textYOffset;
+				textXOffset = textXOffset;
+				textYOffset = textYOffset;
 			}
 			float GetX() override
 			{
-				return this->x;
+				return x;
 			}
 			float GetY() override
 			{
-				return this->y;
+				return y;
 			}
 			bool IsChecked() {
 				return Checked;
@@ -571,9 +625,9 @@ namespace olc
 			{
 				Checked = !Checked;
 			}
-			void UpdateControl() override
+			void UpdateSelf() override
 			{
-				CBoundingBox* boundingBox = CreateBoundingBox(x, y, 20, 20);
+				CBox* boundingBox = CreateBox(x, y, 20, 20);
 				if (IsInBounds(boundingBox, pge->GetMouseX(), pge->GetMouseY())) {
 					if (pge->GetMouse(0).bPressed || pge->GetMouse(1).bPressed || pge->GetMouse(2).bPressed) {
 						Checked = !Checked;
@@ -595,9 +649,9 @@ namespace olc
 				}
 
 				if (Checked)
-					pge->DrawString(this->x + 3, this->y + 3, "X", this->selectionColor, 2);
-				pge->DrawRect(this->x, this->y, 20, 20, this->outlineColor);
-				pge->DrawString(this->x + this->textXOffset, this->y + this->textYOffset, this->text, textColor, this->textScale);
+					pge->DrawString(x + 3, y + 3, "X", selectionColor, 2);
+				pge->DrawRect(x, y, 20, 20, outlineColor);
+				pge->DrawString(x + textXOffset, y + textYOffset, text, textColor, textScale);
 				delete boundingBox;
 			}
 		};
@@ -605,7 +659,7 @@ namespace olc
 		/* Similar to a slider but in a circle */
 		class CSpinner : public BasicControl
 		{
-		private:
+		public:
 			olc::Pixel outlineColor;
 			olc::Pixel dotColor;
 			olc::Pixel backgroundColor;
@@ -617,7 +671,6 @@ namespace olc
 			float angle = 0.0f;
 			float radius = 20;
 			int32_t lastValue = 0;
-		public:
 			CSpinner(olc::vf2d position = olc::vf2d(0, 0), float radius = 0.0f, float dotSize = 0.0f, olc::Pixel outlineColor = olc::Pixel(236, 240, 241), olc::Pixel dotColor = olc::Pixel(46, 204, 113), olc::Pixel bgColor = olc::Pixel(0, 0, 0))
 			{
 				this->outlineColor = outlineColor;
@@ -648,13 +701,17 @@ namespace olc
 			{
 				return angle;
 			}
+			void SetAngle(float angle)
+			{
+				angle = angle;
+			}
 			const float MaxAngle()
 			{
 				return 133.0f;
 			}
-			void UpdateControl() override
+			void UpdateSelf() override
 			{
-				CBoundingBox* boundingBox = CreateBoundingBox(x - radius, y - radius, radius * 2, radius * 2);
+				CBox* boundingBox = CreateBox(x - radius, y - radius, radius * 2, radius * 2);
 				if (IsInBounds(boundingBox, pge->GetMouseX(), pge->GetMouseY()))
 				{
 					H_CHover(this);
