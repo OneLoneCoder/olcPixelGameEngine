456534fd16678a76ed1e51c46f34c8c84b1ebd1d
Tim Varley <tim.varley@gmail.com>
2021-06-17 15:29:01 -0400

    Place guard around UNUSED defined

--- olcPixelGameEngine.h
+++ olcPixelGameEngine.h
@@ -394,7 +394,9 @@ int main()
 	#define olcT(s) s
 #endif
 
-#define UNUSED(x) (void)(x)
+#if ! defined(UNUSED)
+  #define UNUSED(x) (void)(x)
+#endif
 
 // O------------------------------------------------------------------------------O
 // | PLATFORM SELECTION CODE, Thanks slavka!                                      |
